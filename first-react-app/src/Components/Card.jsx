376663f1dff596b3d08be34c32b13c7b14acaca0
Etteja Hoque <ettejahoque2003@gmail.com>
2025-07-23 00:01:14 +0530

    Lecture-1 Componentes

---
+++ first-react-app/src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Card.css"
+const Card = () => {
+  return (
+    <div className='card'>
+      <h1>Title:Dog</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate incidunt aperiam, 
+        possimus veritatis nihil excepturi doloremque et perferendis. Assumenda maxime voluptatem officiis cupiditate id quo nemo eos nobis nesciunt vero.</p>
+    </div>
+  )
+}
+
+export default Card
