1905d5d64057db83f5127a3d2492eb8ce1723d24
Etteja Hoque <ettejahoque2003@gmail.com>
2025-07-24 22:47:37 +0530

    Creat my first react app

---
+++ react-app-one/src/Components/Container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Container.css"
+const Container = () => {
+  return (
+    <div className='container'>
+      <h1>Etteja Hoque</h1>
+      <p>This is my first react app,
+        I am just learning react js.This is a my first react app.
+        I am very excited to build react app.
+      </p>
+    </div>
+  )
+}
+
+export default Container
